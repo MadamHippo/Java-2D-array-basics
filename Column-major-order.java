6dc3a3751e754e3a4aa81d22c53d98ff73bf20e7
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-10-01 00:37:25 -0700

    Update Column-major-order.java

--- Column-major-order.java
+++ Column-major-order.java
@@ -24,3 +24,40 @@ public class ColumnMajor {
 }
 
 
+/* 
+
+
+PRINTS
+
+Output:
+Lap index: 0, Time index: 0
+Lap index: 0, Time index: 1
+Lap index: 0, Time index: 2
+Sum of lap 0 times: 205.212
+Average time for lap 0: 68.404
+Lap index: 1, Time index: 0
+Lap index: 1, Time index: 1
+Lap index: 1, Time index: 2
+Sum of lap 1 times: 222.95499999999998
+Average time for lap 1: 74.31833333333333
+Lap index: 2, Time index: 0
+Lap index: 2, Time index: 1
+Lap index: 2, Time index: 2
+Sum of lap 2 times: 213.645
+Average time for lap 2: 71.215
+Lap index: 3, Time index: 0
+Lap index: 3, Time index: 1
+Lap index: 3, Time index: 2
+Sum of lap 3 times: 208.82700000000003
+Average time for lap 3: 69.60900000000001
+Lap index: 4, Time index: 0
+Lap index: 4, Time index: 1
+Lap index: 4, Time index: 2
+Sum of lap 4 times: 212.071
+Average time for lap 4: 70.69033333333333
+Lap index: 5, Time index: 0
+Lap index: 5, Time index: 1
+Lap index: 5, Time index: 2
+Sum of lap 5 times: 229.80899999999997
+Average time for lap 5: 76.603
+*/
